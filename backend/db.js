f78c904badcaea6c0ca30e085434443e8b9e2d85
Chavisky D <auukunn.bkk@gmail.com>
2026-01-03 14:42:10 +0700

    fix db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
-const { pool } = require('pg');
+const { Pool } = require("pg");
 
-const pool = new pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
